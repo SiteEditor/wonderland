8859852a258514195b8eedd1f927458f6b9229eb
parsaatef <parsaatef@gmail.com>
2017-08-30 19:24:04 +0430

    module

--- site-editor/modules/rocket/skins/default/sed_rocket_animate.php
+++ site-editor/modules/rocket/skins/default/sed_rocket_animate.php
@@ -1,7 +1,7 @@
 <div <?php echo $sed_attrs; ?> class="module module-rocket module-rocket-default <?php echo $class; ?> ">
 
     <section id="rocket" class="clearfix">
-        <p class='in' id="rocketP"><?php $content;?></p>
+        <p class='in' id="rocketP"><?php echo $content;?></p>
         <div class='l' id='layer3'></div>
         <div class='l' id='layer2'></div>
         <div class='l' id='layer1'></div>
